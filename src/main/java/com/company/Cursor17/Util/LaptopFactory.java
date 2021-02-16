08ca1126f09a7a966a34d70bfc9b814e6b24cc6a
maksim-protsenko <protsenko.maksim.88@gmail.com>
2021-02-16 22:51:43 +0200

    used collections instead of arrays, use one instance of Random instead of 8 ones

--- src/main/java/com/company/Cursor17/Util/LaptopFactory.java
+++ src/main/java/com/company/Cursor17/Util/LaptopFactory.java
@@ -2,34 +2,35 @@ package com.company.Cursor17.Util;
 
 import com.company.Cursor17.Model.Laptop;
 
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class LaptopFactory {
-    private static final String[] brands = new String[]{"Lenovo", "Acer", "Dell"};
-    private static final String[] models = new String[]{"Alienware M15", "ThinkPad X1 Carbon", "Legion 5", "Nitro 5"};
-    private static final String[] cpuBrands = new String[]{"Intel", "AMD"};
-    private static final String[] cpuModels = new String[]{"Ryzen 5 5600U", "Ryzen 7 5700U", "Ryzen 9 5900HX", "i5-10210U", "i7-10510U", "i9-10980HK"};
-    private static final int[] rams = new int[]{4, 8, 16, 32, 64, 128};
-    private static final int[] hddCapacityMb = new int[]{500, 1000, 2000, 3000};
-    private static final String[] materials = new String[]{"polycarbonate ABS", "aluminium alloy", "magnesium alloy", "carbon fiber"};
+
+    private static final List<String> brands = new ArrayList<String>(Arrays.asList("Lenovo", "Acer", "Dell"));
+    private static final List<String> models = new ArrayList<String>(Arrays.asList("Alienware M15", "ThinkPad X1 Carbon", "Legion 5", "Nitro 5"));
+    private static final List<String> cpuBrands = new ArrayList<String>(Arrays.asList("Intel", "AMD"));
+    private static final List<String> cpuModels = new ArrayList<String>(Arrays.asList("Ryzen 5 5600U", "Ryzen 7 5700U", "Ryzen 9 5900HX", "i5-10210U", "i7-10510U", "i9-10980HK"));
+    private static final List<Integer> rams = new ArrayList<Integer>(Arrays.asList(4, 8, 16, 32, 64, 128));
+    private static final List<Integer> hddCapacityMb = new ArrayList<Integer>(Arrays.asList(500, 1000, 2000, 3000));
+    private static final List<String> materials = new ArrayList<String>(Arrays.asList("polycarbonate ABS", "aluminium alloy", "magnesium alloy", "carbon fiber"));
     private static final Date startDateOfProduction = new GregorianCalendar(2018, 1, 1).getTime();
     private static final Date endDateOfProduction = new GregorianCalendar(2021, 1, 28).getTime();
 
     public static Laptop createLaptop() {
         Laptop laptop = new Laptop();
-        laptop.setBrand(brands[new Random().nextInt(2)]);
-        laptop.setModel(models[new Random().nextInt(3)]);
-        laptop.setCpuBrand(cpuBrands[new Random().nextInt(1)]);
-        laptop.setCpuModel(cpuModels[new Random().nextInt(5)]);
-        laptop.setRam(rams[new Random().nextInt(5)]);
-        laptop.setHddCapacity(hddCapacityMb[new Random().nextInt(3)]);
-        laptop.setCaseMaterial(materials[new Random().nextInt(3)]);
-        laptop.setSecondHand(new Random().nextBoolean());
-        laptop.setPrice((int)(Math.random() * 35000) + 25000);
-        laptop.setProductionDate(between(startDateOfProduction,endDateOfProduction));
+        Random randomizer = new Random();
+
+        laptop.setBrand(brands.get(randomizer.nextInt(brands.size())));
+        laptop.setModel(models.get(randomizer.nextInt(models.size())));
+        laptop.setCpuBrand(cpuBrands.get(randomizer.nextInt(cpuBrands.size())));
+        laptop.setCpuModel(cpuModels.get(randomizer.nextInt(cpuModels.size())));
+        laptop.setRam(rams.get(randomizer.nextInt(rams.size())));
+        laptop.setHddCapacity(hddCapacityMb.get(randomizer.nextInt(hddCapacityMb.size())));
+        laptop.setCaseMaterial(materials.get(randomizer.nextInt(materials.size())));
+        laptop.setSecondHand(randomizer.nextBoolean());
+        laptop.setPrice((int) (Math.random() * 35000) + 25000);
+        laptop.setProductionDate(between(startDateOfProduction, endDateOfProduction));
         return laptop;
     }
 
